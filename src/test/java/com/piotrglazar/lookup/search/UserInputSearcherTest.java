ade17e34c197942ab4c7d16a1733bba8e5966c4d
Piotr Glazar <piotr.glazar@gmail.com>
2015-02-22 22:23:19 +0100

    Missing UserInputSearcher test

--- src/test/java/com/piotrglazar/lookup/search/UserInputSearcherTest.java
+++ src/test/java/com/piotrglazar/lookup/search/UserInputSearcherTest.java
@@ -1,6 +1,7 @@
 package com.piotrglazar.lookup.search;
 
 import com.piotrglazar.lookup.utils.ConsoleReader;
+import org.apache.lucene.queryparser.classic.ParseException;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
@@ -10,6 +11,7 @@ import org.mockito.runners.MockitoJUnitRunner;
 import java.io.IOException;
 
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyZeroInteractions;
 
 @RunWith(MockitoJUnitRunner.class)
@@ -34,5 +36,17 @@ public class UserInputSearcherTest {
 
         // then
         verifyZeroInteractions(searcher);
+    }
+
+    @Test
+    public void shouldSearchUsingQueryProvidedByUser() throws IOException, ParseException {
+        // given
+        given(consoleReader.readLine()).willReturn("query", UserInputSearcher.STOP);
+
+        // when
+        userInputSearcher.search();
+
+        // then
+        verify(searcher).searchInEnglish("query");
     }
 }
